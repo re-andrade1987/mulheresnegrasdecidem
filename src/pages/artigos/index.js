a86f9700ce26f116c1f580ec53e1dffbdfd96cd9
re-andrade1987 <renataandrade1987@hotmail.com>
2020-07-21 23:30:45 -0300

    Ajustando pagina artigos

--- src/pages/artigos/index.js
+++ src/pages/artigos/index.js
@@ -17,31 +17,40 @@ function Artigos() {
             <div className='box-conteudo'>
                 <img src={artigoImg} alt="logo" />
                 <div className='box-texts-artigo'>
-                    <h3>Apoiadores de Bolsonaro realizam pelo menos 50 ataques em todo o país</h3>
-                    <p>
-                        commodo pulvinaar,convallis aliquam pharetra eleifend tincidunt odio nam vehicula, metus nibh aenean sit cras adipiscing luctus.
-                    </p>
+                    <h3>Corpos que pautam: a relação entre representatividade e pautas legislativas </h3>
+                    <p>7/7/2020 Estadão Juliana Marques </p>
                     <div className="icon-redes-nav">
                         <a target='blank' href='https://twitter.com/MNdecidem'><img src={twitter} alt="logo-twitter" /></a>
                         <a target='blank' href='https://www.facebook.com/mulheresnegrasdecidem'><img src={facebook} alt="logo-facebook" /></a>
                         <a target='blank' href='https://www.instagram.com/mulheresnegrasdecidem/'><img src={instagram} alt="logo-facebook" /></a>
                     </div>
-                    <a className='link-podcast' href='https://twitter.com/MNdecidem'>+ Leia Mais</a>
+                    <a className='link-podcast' href='https://politica.estadao.com.br/blogs/legis-ativo/corpos-que-pautam-a-relacao-entre-representatividade-e-pautas-legislativas/'>+ Leia Mais</a>
                 </div>
             </div>
             <div className='box-conteudo'>
                 <img src={artigoImg} alt="logo" />
                 <div className='box-texts-artigo'>
-                    <h3>Apoiadores de Bolsonaro realizam pelo menos 50 ataques em todo o país</h3>
-                    <p>
-                        gravida neque at augue leo euismod, potenti vel tempus quis proin tincidunt consectetur id, aptent cubilia tempus turpis justo lacinia placerat aptent.
-                    </p>
+                    <h3>Mulheres negras na dianteira da transição que precisamos</h3>
+                    <p>14/04/20 Cult Laura Astrolabio</p>
                     <div className="icon-redes-nav">
                         <a target='blank' href='https://twitter.com/MNdecidem'><img src={twitter} alt="logo-twitter" /></a>
                         <a target='blank' href='https://www.facebook.com/mulheresnegrasdecidem'><img src={facebook} alt="logo-facebook" /></a>
                         <a target='blank' href='https://www.instagram.com/mulheresnegrasdecidem/'><img src={instagram} alt="logo-facebook" /></a>
                     </div>
-                    <a className='link-podcast' target='blank' href='https://twitter.com/MNdecidem'>+ Leia Mais</a>
+                    <a className='link-podcast' target='blank' href='https://revistacult.uol.com.br/home/mulheres-negras-na-dianteira-da-transicao-que-precisamos/'>+ Leia Mais</a>
+                </div>
+            </div>
+            <div className='box-conteudo'>
+                <img src={artigoImg} alt="logo" />
+                <div className='box-texts-artigo'>
+                    <h3>Como garantir o acesso das mulheres à Política</h3>
+                    <p>1/9/2019 Carta Capital Laura Astrolabio</p>
+                    <div className="icon-redes-nav">
+                        <a target='blank' href='https://twitter.com/MNdecidem'><img src={twitter} alt="logo-twitter" /></a>
+                        <a target='blank' href='https://www.facebook.com/mulheresnegrasdecidem'><img src={facebook} alt="logo-facebook" /></a>
+                        <a target='blank' href='https://www.instagram.com/mulheresnegrasdecidem/'><img src={instagram} alt="logo-facebook" /></a>
+                    </div>
+                    <a className='link-podcast' target='blank' href='https://www.cartacapital.com.br/justica/como-garantir-o-acesso-das-mulheres-a-politica/'>+ Leia Mais</a>
                 </div>
             </div>
         </section>
